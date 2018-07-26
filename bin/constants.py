d1f3c2a386f9fcbcaaf194d547628802c001efc9
Mališa Vučinić <malishav@gmail.com>
2018-07-26 17:17:43 +0200

    COAP-40: First example implementation of ACE client
    Assumes Authorization Server (AS) is listening on a loopback interface.
    Client requests access to SCOPE resource without knowing where this
    resource is located. The IPv6 address of the RS is communicated to the
    client in the audience parameter of the response. Upon reception of the
    response, client installs a new OSCORE context and uses it to access the
    SCOPE resource on the RS.

---
+++ bin/constants.py
@@ -0,0 +1,112 @@
+# constants from draft-ietf-ace-oauth-authz-13
+
+# Figure 12 draft-ietf-ace-oauth-authz-13: CBOR mappings used in token requests
+ACE_PARAMETERS_LABELS_AUD = 3  # text string
+ACE_PARAMETERS_LABELS_CLIENT_ID = 8  # text string
+ACE_PARAMETERS_LABELS_CLIENT_SECRET = 9  # byte string
+ACE_PARAMETERS_LABELS_RESPONSE_TYPE = 10  # text string
+ACE_PARAMETERS_LABELS_REDIRECT_URI = 11  # text string
+ACE_PARAMETERS_LABELS_SCOPE = 12  # text or byte string
+ACE_PARAMETERS_LABELS_STATE = 13  # text string
+ACE_PARAMETERS_LABELS_CODE = 14  # byte string
+ACE_PARAMETERS_LABELS_ERROR = 15  # unsigned integer
+ACE_PARAMETERS_LABELS_ERROR_DESCRIPTION = 16  # text string
+ACE_PARAMETERS_LABELS_ERROR_URI = 17  # text string
+ACE_PARAMETERS_LABELS_GRANT_TYPE = 18  # unsigned integer
+ACE_PARAMETERS_LABELS_ACCESS_TOKEN = 19  # byte string
+ACE_PARAMETERS_LABELS_TOKEN_TYPE = 20  # unsigned integer
+ACE_PARAMETERS_LABELS_EXPIRES_IN = 21  # unsigned integer
+ACE_PARAMETERS_LABELS_USERNAME = 22  # text string
+ACE_PARAMETERS_LABELS_PASSWORD = 23  # text string
+ACE_PARAMETERS_LABELS_REFRESH_TOKEN = 24  # byte string
+ACE_PARAMETERS_LABELS_CNF = 25  # map
+ACE_PARAMETERS_LABELS_PROFILE = 26  # unsigned integer
+ACE_PARAMETERS_LABELS_RS_CNF = 31  # map
+
+#  Figure 11 from draft-ietf-ace-oauth-authz-13: CBOR abbreviations for common grant types
+ACE_CBOR_ABBREVIATIONS_PASSWORD = 0
+ACE_CBOR_ABBREVIATIONS_AUTHORIZATION_CODE = 1
+ACE_CBOR_ABBREVIATIONS_CLIENT_CREDENTIALS = 2
+ACE_CBOR_ABBREVIATIONS_REFRESH_TOKEN = 3
+
+ACE_ACCESS_TOKEN_TYPE_BEARER = 1
+ACE_ACCESS_TOKEN_TYPE_POP = 2
+
+# from https://tools.ietf.org/html/draft-ietf-ace-cwt-proof-of-possession-03#section-3.1
+ACE_CWT_CNF_COSE_KEY = 1
+ACE_CWT_CNF_ENCRYPTED_COSE_KEY = 2
+ACE_CWT_CNF_KID = 3
+
+# values from RFC8152
+
+# COSE key labels
+COSE_KEY_LABEL_KTY                          = 1
+COSE_KEY_LABEL_KID                          = 2
+COSE_KEY_LABEL_ALG                          = 3
+COSE_KEY_LABEL_KEYOPS                       = 4
+COSE_KEY_LABEL_BASEIV                       = 5
+COSE_KEY_LABEL_K                            = -1
+COSE_KEY_LABEL_CLIENT_ID                    = 6      # value TBD by IANA, registered in draft-ietf-ace-oscore-profile-02
+COSE_KEY_LABEL_SERVER_ID                    = 7      # value TBD by IANA, registered in draft-ietf-ace-oscore-profile-02
+COSE_KEY_LABEL_KDF                          = 8      # value TBD by IANA, registered in draft-ietf-ace-oscore-profile-02
+COSE_KEY_LABEL_SLT                          = 9      # value TBD by IANA, registered in draft-ietf-ace-oscore-profile-02
+COSE_KEY_LABEL_ALL = [
+    COSE_KEY_LABEL_KTY,
+    COSE_KEY_LABEL_KID,
+    COSE_KEY_LABEL_ALG,
+    COSE_KEY_LABEL_KEYOPS,
+    COSE_KEY_LABEL_BASEIV,
+    COSE_KEY_LABEL_K,
+    COSE_KEY_LABEL_CLIENT_ID,
+    COSE_KEY_LABEL_SERVER_ID,
+    COSE_KEY_LABEL_KDF,
+    COSE_KEY_LABEL_SLT,
+]
+
+# COSE key values
+COSE_KEY_VALUE_OKP                          = 1
+COSE_KEY_VALUE_EC2                          = 2
+COSE_KEY_VALUE_SYMMETRIC                    = 4
+COSE_KEY_VALUE_ALL = [
+    COSE_KEY_VALUE_OKP,
+    COSE_KEY_VALUE_EC2,
+    COSE_KEY_VALUE_SYMMETRIC,
+]
+
+COSE_ALG_AES_CCM_16_64_128                  = 10
+COSE_ALG_AES_CCM_16_64_256                  = 11
+COSE_ALG_AES_CCM_64_64_128                  = 12
+COSE_ALG_AES_CCM_64_64_256                  = 13
+COSE_ALG_AES_CCM_16_128_128                 = 30
+COSE_ALG_AES_CCM_16_128_256                 = 31
+COSE_ALG_AES_CCM_64_128_128                 = 32
+COSE_ALG_AES_CCM_64_128_256                 = 33
+
+COSE_ALG_AES_CCM_ALL = [
+    COSE_ALG_AES_CCM_16_64_128,
+    COSE_ALG_AES_CCM_16_64_256,
+    COSE_ALG_AES_CCM_64_64_128,
+    COSE_ALG_AES_CCM_64_64_256,
+    COSE_ALG_AES_CCM_16_128_128,
+    COSE_ALG_AES_CCM_16_128_256,
+    COSE_ALG_AES_CCM_64_128_128,
+    COSE_ALG_AES_CCM_64_128_256,
+]
+
+COSE_COMMON_HEADER_PARAMETERS_ALG                = 1
+COSE_COMMON_HEADER_PARAMETERS_CRIT               = 2
+COSE_COMMON_HEADER_PARAMETERS_CONTENT_TYPE       = 3
+COSE_COMMON_HEADER_PARAMETERS_KID                = 4
+COSE_COMMON_HEADER_PARAMETERS_IV                 = 5
+COSE_COMMON_HEADER_PARAMETERS_PIV                = 6
+COSE_COMMON_HEADER_PARAMETERS_COUNTER_SIGNATURE  = 7
+
+COSE_COMMON_HEADER_PARAMETERS_ALL = [
+    COSE_COMMON_HEADER_PARAMETERS_ALG,
+    COSE_COMMON_HEADER_PARAMETERS_CRIT,
+    COSE_COMMON_HEADER_PARAMETERS_CONTENT_TYPE,
+    COSE_COMMON_HEADER_PARAMETERS_KID,
+    COSE_COMMON_HEADER_PARAMETERS_IV,
+    COSE_COMMON_HEADER_PARAMETERS_PIV,
+    COSE_COMMON_HEADER_PARAMETERS_COUNTER_SIGNATURE,
+]
---
+++ bin/test_ace_client.py
@@ -0,0 +1,117 @@
+import os
+import sys
+here = sys.path[0]
+sys.path.insert(0, os.path.join(here,'..'))
+
+import time
+import binascii
+import cbor
+
+from coap import coap
+from coap import coapOption           as o
+from coap import coapObjectSecurity   as oscoap
+from coap import coapDefines          as d
+from coap import coapUtils            as u
+
+import constants
+
+import logging_setup
+
+AS_IP = '::1'
+SCOPE = 'resource1'
+AUTHZ_INFO = 'authz-info'
+
+# open
+c = coap.coap(udpPort=5000)
+
+context = oscoap.SecurityContext(masterSecret=binascii.unhexlify('000102030405060708090A0B0C0D0E0F'),
+                                 senderID=binascii.unhexlify('141592cc00000001'),
+                                 recipientID='JRC',
+                                 aeadAlgorithm=oscoap.AES_CCM_16_64_128())
+
+objectSecurity = o.ObjectSecurity(context=context)
+
+contentFormat = o.ContentFormat(cformat=[d.FORMAT_CBOR])
+
+try:
+
+    # Step 1: Request authorization from the AS to access "resource1"
+    request_payload = {}
+    request_payload[constants.ACE_PARAMETERS_LABELS_GRANT_TYPE] = constants.ACE_CBOR_ABBREVIATIONS_CLIENT_CREDENTIALS
+    request_payload[constants.ACE_PARAMETERS_LABELS_SCOPE] = unicode(SCOPE)
+
+    print '====== Request payload ======'
+    print binascii.hexlify(cbor.dumps(request_payload))
+    print '====='
+
+    # obtain an access token
+    (respCode, respOptions, respPayload) = c.POST('coap://[{0}]/token'.format(AS_IP),
+              confirmable=True,
+              options=[contentFormat, objectSecurity],
+              payload=u.str2buf(cbor.dumps(request_payload))
+              )
+
+    payload_hex = u.buf2str(respPayload)
+    print '====== Response payload ======'
+    print binascii.hexlify(payload_hex)
+    print '====='
+
+    # Step 2: Decode the response, install the OSCORE security context and parse the access token for the RS
+    as_response = cbor.loads(payload_hex)
+
+    cnf = as_response[constants.ACE_PARAMETERS_LABELS_CNF]
+
+    cose_key = cnf[constants.ACE_CWT_CNF_COSE_KEY]
+
+    if cose_key[constants.COSE_KEY_LABEL_KTY] != constants.COSE_KEY_VALUE_SYMMETRIC:
+        raise NotImplementedError
+
+    if cose_key.get(constants.COSE_KEY_LABEL_ALG, constants.COSE_ALG_AES_CCM_16_64_128) != constants.COSE_ALG_AES_CCM_16_64_128:
+        raise NotImplementedError
+    else:
+        aeadAlgo = oscoap.AES_CCM_16_64_128()
+
+    context_c_rs = oscoap.SecurityContext(
+        masterSecret=cose_key.get(constants.COSE_KEY_LABEL_K),
+        senderID=cose_key.get(constants.COSE_KEY_LABEL_CLIENT_ID),
+        recipientID=cose_key.get(constants.COSE_KEY_LABEL_SERVER_ID),
+        masterSalt=cose_key.get(constants.COSE_KEY_LABEL_SLT, ""),
+        aeadAlgorithm=aeadAlgo,
+    )
+
+    access_token = as_response[constants.ACE_PARAMETERS_LABELS_ACCESS_TOKEN]
+
+    audience = as_response.get(constants.ACE_PARAMETERS_LABELS_AUD, AS_IP)  # if audience is not given, what RS should we default to?
+
+    # Step 3: POST the access token to the RS over unprotected channel
+    (respCode, respOptions, respPayload) = c.POST('{0}/{1}'.format(audience, AUTHZ_INFO),
+              confirmable=True,
+              options=[],
+              payload=u.str2buf(access_token)
+              )
+
+    if respCode != d.COAP_RC_2_01_CREATED:
+        raise NotImplementedError
+
+    # Step 4: Request the resource over OSCORE
+    oscore = o.ObjectSecurity(context=context_c_rs)
+    (respCode, respOptions, respPayload) = c.GET('{0}/{1}'.format(audience, SCOPE),
+                                                  confirmable=True,
+                                                  options=[oscore],
+                                                  )
+
+    print '===== GET to {0}/{1} returned ====='.format(audience, SCOPE)
+    print ''.join([chr(b) for b in respPayload])
+    print '====='
+
+# this includes CoAP errors
+except Exception as err:
+    print err
+
+
+# close
+c.close()
+
+time.sleep(0.500)
+
+raw_input("Done. Press enter to close.")
